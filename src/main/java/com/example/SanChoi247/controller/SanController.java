3a25cfc44698ee036b15ed26850569c2c5f4162e
dangthanhvinh003 <dvih003@gmail.com>
2024-10-06 21:02:03 +0700

    update view detaillocation

--- src/main/java/com/example/SanChoi247/controller/SanController.java
+++ src/main/java/com/example/SanChoi247/controller/SanController.java
@@ -1,6 +1,8 @@
 package com.example.SanChoi247.controller;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,13 +10,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.example.SanChoi247.model.entity.Booking;
 import com.example.SanChoi247.model.entity.San;
+import com.example.SanChoi247.model.entity.ScheduleBooking;
+import com.example.SanChoi247.model.repo.BookingRepo;
 import com.example.SanChoi247.model.repo.SanRepo;
+import com.example.SanChoi247.model.repo.ScheduleBookingRepo;
 
 @Controller
 public class SanController {
     @Autowired
     SanRepo sanRepo;
+    @Autowired
+    ScheduleBookingRepo scheduleBookingRepo;
 
     @GetMapping("/ViewDetail/{id}")
     public String viewDetail(@PathVariable("id") int uid, Model model) throws Exception {
@@ -23,4 +31,17 @@ public class SanController {
         model.addAttribute("SanDetail", sanList);
         return "public/viewDeTail";
     }
+    @GetMapping("/ShowDetailLocation/{id}")
+    public String showDetailLocation(@PathVariable("id") int sid, Model model) throws Exception {
+        San san = sanRepo.getSanById(sid);
+        List<ScheduleBooking> bookings = scheduleBookingRepo.getAvailableBookings(sid);
+
+        // Thêm dữ liệu vào model để hiển thị trên view
+        model.addAttribute("bookings", bookings);
+        model.addAttribute("SanDetail", san);
+        return "public/detailLocation";
+    }
+ 
 }
+
+
